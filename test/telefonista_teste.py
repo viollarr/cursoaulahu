687d64c6bdc4aff4123133d19d6782e66c63a82c
Victor Narcizo <viollarr@gmail.com>
2016-05-07 13:15:14 -0300

    injeção de dependência no teste utilizando uma class mocada

--- test/telefonista_teste.py
+++ test/telefonista_teste.py
@@ -7,7 +7,12 @@ diretorio_huaula = path.join(diretorio_desse_arquivo, '..')
 diretorio_huaula = path.abspath(diretorio_huaula)
 sys.path.append(diretorio_huaula)
 
-from cursoaulahu.telefonista import Telefonista
+from cursoaulahu.telefonista import Telefonista, Telefone
+
+
+class TelefoneMoc(Telefone):
+    def telefonar(self, numero):
+        return 'Tel fake para {}'.format(numero)
 
 
 class TelefonistaTeste(unittest.TestCase):
@@ -27,20 +32,22 @@ class TelefonistaTeste(unittest.TestCase):
 
     def test_telefonar_um_contato(self):
         telefonista = Telefonista()
+        telefonista._telefone = TelefoneMoc()
         contato = ('Victor', '2345678')
         telefonista.adicionar_contato(*contato)
         resultado_da_ligacao = telefonista.ligar()
-        self.assertEqual('Contato Victor, Telefonando para 2345678', resultado_da_ligacao)
+        self.assertEqual('Contato Victor, Tel fake para 2345678', resultado_da_ligacao)
 
     def test_telefonar_dois_contatos(self):
         telefonista = Telefonista()
+        telefonista._telefone = TelefoneMoc()
         victor = ('Victor', '2345678')
         renzo = ('Renzo', '8765432')
         telefonista.adicionar_contato(*victor)
         telefonista.adicionar_contato(*renzo)
         resultado_da_ligacao = telefonista.ligar()
-        self.assertEqual('Contato Victor, Telefonando para 2345678\nContato Renzo, Telefonando para 8765432', resultado_da_ligacao)
-
+        self.assertEqual('Contato Victor, Tel fake para 2345678\nContato Renzo, Tel fake para 8765432',
+                         resultado_da_ligacao)
 
 
 if __name__ == '__main__':
