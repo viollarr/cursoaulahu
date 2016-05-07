9513c2283df7963c9947c90692ceb6b6c87a1fee
Victor Narcizo <viollarr@gmail.com>
2016-05-07 11:20:24 -0300

    teste utilizando unittest

---
+++ test/soma_teste.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+def soma(param, param1):
+    return param + param1
+
+
+class SomaTestes(unittest.TestCase):
+
+    def test_soma_com_zeros(self):
+        self.assert_resultado_de_soma(0, 0, 0)
+
+    def assert_resultado_de_soma(self, resultado_esperado, operando1, operando2):
+        resultado_calculado = soma(operando1, operando2)
+        self.assertEqual(resultado_esperado, resultado_calculado)
+
+    def test_soma_com_positivo(self):
+        self.assert_resultado_de_soma(3, 1, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
